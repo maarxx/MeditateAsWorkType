21cc8786721bf3d46aa2610a6a34df05fdfba9dc
maarxx <chris.lewark@gmail.com>
2020-10-16 23:41:53 -0400

    Add project files.

---
+++ MeditateAsWorkType/WorkGiver_Meditate.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace MeditateAsWorkType
+{
+    public class WorkGiver_Meditate : WorkGiver
+    {
+        public override Job NonScanJob(Pawn pawn)
+        {
+            bool shouldMeditate = pawn.HasPsylink && pawn.psychicEntropy.CurrentPsyfocus < pawn.psychicEntropy.TargetPsyfocus;
+            if (shouldMeditate)
+            {
+                return MeditationUtility.GetMeditationJob(pawn);
+            }
+            return null;
+        }
+    }
+}
