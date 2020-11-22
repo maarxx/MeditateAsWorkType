9f7b73b9c5d8cfae4bb853fad32a169b89593ee8
maarxx <chris.lewark@gmail.com>
2020-11-21 23:35:51 -0500

    Fixed Meditate for Anima Grass

--- MeditateAsWorkType/WorkGiver_MeditateForAnima.cs
+++ MeditateAsWorkType/WorkGiver_MeditateForAnima.cs
@@ -24,16 +24,13 @@ namespace MeditateAsWorkType
             {
                 Job job = JobMaker.MakeJob(JobDefOf.Meditate, meditationSpotAndFocus.spot, null, meditationSpotAndFocus.focus);
                 job.ignoreJoyTimeAssignment = !forJoy;
-                Log.Message("GetAnimaMeditationJob has job");
                 return job;
             }
-            Log.Message("GetAnimaMeditationJob has NO job");
             return null;
         }
 
         public static MeditationSpotAndFocus FindAnimaMeditationSpot(Pawn pawn)
         {
-            float num = float.MinValue;
             LocalTargetInfo spot = LocalTargetInfo.Invalid;
             LocalTargetInfo focus = LocalTargetInfo.Invalid;
             if (!ModLister.RoyaltyInstalled)
@@ -41,47 +38,15 @@ namespace MeditateAsWorkType
                 Log.ErrorOnce("Psyfocus meditation is a Royalty-specific game system. If you want to use this code please check ModLister.RoyaltyInstalled before calling it.", 657324);
                 return new MeditationSpotAndFocus(spot, focus);
             }
-            Room ownedRoom = pawn.ownership.OwnedRoom;
-            foreach (LocalTargetInfo item in MeditationUtility.AllMeditationSpotCandidates(pawn))
+            Thing animaTree = pawn.Map.listerThings.ThingsOfDef(DefDatabase<ThingDef>.GetNamed("Plant_TreeAnima")).First();
+            CompMeditationFocus compMeditationFocus = animaTree.TryGetComp<CompMeditationFocus>();
+            if (compMeditationFocus != null && compMeditationFocus.CanPawnUse(pawn))
             {
-                if (MeditationUtility.SafeEnvironmentalConditions(pawn, item.Cell, pawn.Map))
-                {
-                    //LocalTargetInfo localTargetInfo = (item.Thing is Building_Throne) ? ((LocalTargetInfo)item.Thing) : MeditationUtility.BestFocusAt(item, pawn);
-                    LocalTargetInfo localTargetInfo = MeditationUtility.BestFocusAt(item, pawn);
-                    float num2 = 1f / Mathf.Max(item.Cell.DistanceToSquared(pawn.Position), 0.1f);
-                    //if (pawn.HasPsylink && localTargetInfo.IsValid)
-                    if (!(localTargetInfo.HasThing && localTargetInfo.Thing.def.defName == "Plant_TreeAnima"))
-                    {
-                        continue;
-                    }
-                    if (localTargetInfo.IsValid)
-                    {
-                        num2 += localTargetInfo.Thing.GetStatValueForPawn(StatDefOf.MeditationFocusStrength, pawn) * 100f;
-                    }
-                    Room room = item.Cell.GetRoom(pawn.Map);
-                    if (room != null && ownedRoom == room)
-                    {
-                        num2 += 1f;
-                    }
-                    Building building;
-                    if (item.Thing != null && (building = (item.Thing as Building)) != null && building.GetAssignedPawn() == pawn)
-                    {
-                        num2 += (float)((building.def == ThingDefOf.MeditationSpot) ? 200 : 100);
-                    }
-                    if (!item.Cell.Standable(pawn.Map))
-                    {
-                        num2 = float.NegativeInfinity;
-                    }
-                    if (num2 > num)
-                    {
-                        spot = item;
-                        focus = localTargetInfo;
-                        num = num2;
-                    }
-                }
+                spot = MeditationUtility.MeditationSpotForFocus(animaTree, pawn);
+                focus = animaTree;
+                return new MeditationSpotAndFocus(spot, focus);
             }
             return new MeditationSpotAndFocus(spot, focus);
         }
-
     }
 }
